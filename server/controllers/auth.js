d5a134715f90cf16134c7e68f113a85b12c12228
thitiwatdb <thitiwat.db@gmail.com>
2024-10-30 22:18:57 +0700

    update

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,7 +5,48 @@ const jwt = require("jsonwebtoken");
 //login
 exports.login = async (req, res) => {
   try {
-    res.send("Hello From login");
+    const { username, password } = req.body;
+
+    //step 1 check username
+    const user = await prisma.User.findFirst({
+      where: {
+        username: username,
+      },
+    });
+    if (!user || !user.status) {
+      return res
+        .status(400)
+        .json({ message: "username not found or not active" });
+    }
+
+    //step 2 check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "password invalid!!!" });
+    }
+
+    //step 3 create payload
+    const payload = {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+      status: user.status,
+    };
+
+    //step 4 generate token
+    jwt.sign(
+      payload,
+      process.env.SECRET,
+      {
+        expiresIn: "1d",
+      },
+      (err, token) => {
+        if (err) {
+          return res.status(500).json({ message: "Server Error" });
+        }
+        res.json({ payload, token });
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server Error" });
