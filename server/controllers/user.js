f6ef7e8c0b724568df139aaa463efb3d6125ded2
thitiwatdb <thitiwat.db@gmail.com>
2024-10-31 01:59:26 +0700

    update

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,67 @@
 exports.listUsers = async (req, res) => {
   try {
     //code
-    res.send("OK");
+    res.send("OK list user");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.changeStatus = async (req, res) => {
+  try {
+    //code
+    res.send("OK change status");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.changeRole = async (req, res) => {
+  try {
+    //code
+    res.send("OK change role");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.userBooking = async (req, res) => {
+  try {
+    //code
+    res.send("OK user booking");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.getUserBooking = async (req, res) => {
+  try {
+    //code
+    res.send("OK get user booking");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.saveUserBooking = async (req, res) => {
+  try {
+    //code
+    res.send("OK save user booking");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.cancelBooking = async (req, res) => {
+  try {
+    //code
+    res.send("OK cancel booking");
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server Error" });
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const { authCheck, adminCheck } = require("../middlewares/authCheck");
-const { listUsers } = require("../controllers/user");
+const {
+  listUsers,
+  changeRole,
+  changeStatus,
+  userBooking,
+  getUserBooking,
+  cancelBooking,
+} = require("../controllers/user");
 
 router.get("/users", authCheck, adminCheck, listUsers);
-router.post("/change-status");
-router.post("/change-role");
+router.post("/change-status", authCheck, adminCheck, changeStatus);
+router.post("/change-role", authCheck, adminCheck, changeRole);
 
-router.post("user/booking");
-router.get("/user/booking");
-router.delete("/user/booking");
+router.post("/user/booking", authCheck, userBooking);
+router.get("/user/booking", authCheck, getUserBooking);
+router.delete("/user/booking", authCheck, cancelBooking);
 
 module.exports = router;
